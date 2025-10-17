dc012965e1e34282ef83049d26f52b50f44f5629
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-10-17 21:32:57 +0300

    (date)

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+package java;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage stage) {
+    Label label = new Label("Salut, Jorj! 🎉 JavaFX merge!");
+    Scene scene = new Scene(label, 400, 200);
+    stage.setScene(scene);
+    stage.setTitle("Test JavaFX");
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    launch();
+  }
+}
